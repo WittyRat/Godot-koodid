2a5bff2fe364b7291c799cac86df8ea55877cfe2
WittyRat <127035631+WittyRat@users.noreply.github.com>
2023-03-05 17:08:10 +0200

    Add files via upload

---
+++ Glock.gd
@@ -0,0 +1,25 @@
+extends "res://PickableObject.gd"
+
+func action():
+	$m1911_finalFire/RootNode/AnimationPlayer.play("Fire")
+	
+	if $Aim.is_colliding():
+		var what_did_we_hit = $Aim.get_collider()
+		if what_did_we_hit.has_method("hit"):
+			what_did_we_hit.hit($Aim.get_collision_point(), $Aim.get_collision_normal())
+			print($Aim.get_collision_point())
+			print($Aim.get_collision_normal())
+
+
+func _process(_delta):
+	if $Aim.is_colliding():
+		var whatt_did_we_hit = $Aim.is_colliding()
+		$Aim/TargetDot.global_transform.origin = $Aim.get_collision_point()
+		$Aim/TargetDot.visible = true
+#		Muutuja.Itekst = "täissi sein"
+#		$m1911_finalFire/RootNode/AnimationPlayer.play("Fire")
+#		$AudioStreamPlayer3D.play()
+
+		
+	else:
+		$Aim/TargetDot.visible = false
---
+++ WrongWall.gd
@@ -0,0 +1,13 @@
+extends StaticBody
+
+func _process(delta):
+	# Check for collision with wall using raycast
+	var packed_scene = load("res://PickableObject.tscn")
+	var scene_instance = packed_scene.instance()
+	var aim = scene_instance.get_node("Aim")
+	#print(aim.is_colliding())
+	if aim and aim.is_colliding():
+		var collider = aim.get_collider()
+		if collider is StaticBody and collider.name == "WrongWall":
+			print("Collided with wall!")
+
